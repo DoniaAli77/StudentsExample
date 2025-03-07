9312d147fb095b0412bede9028ce6813ac40b8b3
DoniaAli77 <67129196+DoniaAli77@users.noreply.github.com>
2025-03-07 19:33:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,79 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const studentRouter = require("./Routes/students");
+const studentModel = require("./Models/studentModel"); // * Importing the student model
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/students", studentRouter);
 
-const db_name = "lab2";
+
+// * Get all students
+app.get("/students", async (req, res) => {
+  try {
+    const students = await studentModel.find();
+    return res.status(200).json(students);
+  } catch (err) {
+    return res.status(500).json({ error: err });
+  }
+});
+
+// * Get a student by id
+app.get("/students/:id", async (req, res) => {
+  try {
+    const student = await studentModel.findById(req.params.id);
+    return res.status(200).json(student);
+  } catch (err) {
+    return res.status(500).json({ error: err });
+  }
+});
+
+// * Create a student
+
+app.post("/students", async (req, res) => {
+  try {
+    const newStudent = new studentModel({
+      name: req.body.name,
+      age: req.body.age,
+    });
+    const student = await newStudent.save();
+    return res.status(201).json({student,msg:"created"});
+  } catch (err) {
+    return res.status(500).json({ error: err });
+  }
+});
+
+// * Update a student
+
+app.put("/students/:id", async (req, res) => {
+  try {
+    const student = await studentModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        name: req.body.name,
+        age: req.body.age,
+      },
+      { new: true }
+    );
+    return res.status(200).json({student,msg:"updated"});
+  } catch (error) {
+    return res.status(500).json({ error: err });
+  }
+});
+
+// * Delete a student
+
+app.delete("/students/:id", async (req, res) => {
+  try {
+    const student = await studentModel.findByIdAndDelete(req.params.id);
+    return res.status(200).json({student,msg:"deleted"});
+  } catch (error) {
+    return res.status(500).json({ error: err });
+  }
+});
+
+
+const db_name = "students";
 // * Cloud Connection
 // const db_url = `mongodb+srv://TestUser:TestPassword@cluster0.lfqod.mongodb.net/${db_name}?retryWrites=true&w=majority`;
 // * Local connection

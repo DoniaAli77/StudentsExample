6b3a34bda9ba7a1568cd270eb18899f3c2d415a4
DoniaAli77 <67129196+DoniaAli77@users.noreply.github.com>
2025-03-08 23:27:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const mongoose = require("mongoose");
 const studentModel = require("./Models/studentModel"); // * Importing the student model
 
-app.use(express.json());
+app.use(express.json());// app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 
 
